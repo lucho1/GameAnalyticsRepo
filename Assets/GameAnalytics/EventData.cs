35b1cfb4ffce3402c648541757cb7f17f9765ced
Sergi PR <t3m1X@users.noreply.github.com>
2020-12-16 20:35:05 +0100

    Added generic EventData

---
+++ Assets/GameAnalytics/EventData.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using UnityEngine;
+
+public class EventData : MonoBehaviour
+{
+    uint event_id;
+    DateTime timestamp;
+
+    public string GetJSON() 
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public string[] GetSerialized() 
+    {
+        string[] ret = new string[2];
+
+        ret[0] = event_id.ToString();
+        ret[1] = timestamp.ToString();
+
+        return ret;
+    }
+
+    public string GetSerializedString() 
+    {
+        return event_id.ToString() + ',' + timestamp.ToString();
+    }
+}
